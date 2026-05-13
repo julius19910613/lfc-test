1fca01fc449e1239be777917034194942d4f573c
julius19910613 <julius19910613@users.noreply.github.com>
2026-05-14 04:14:55 +0800

    chore(modernization): 批次2 - webpack 3 → webpack 5
    - 升级 webpack 5 + webpack-dev-server 4 + 现代 loader/plugin，替换 CommonsChunkPlugin、ExtractTextPlugin、UglifyJsPlugin 为 webpack 5 原生能力
    
    实现: OpenAI Codex (gpt-5.4)

--- general-ui/build/webpack.base.conf.js
+++ general-ui/build/webpack.base.conf.js
@@ -1,6 +1,7 @@
 
 
 const path = require('path');
+const webpack = require('webpack');
 const utils = require('./utils');
 const config = require('../config');
 const vueLoaderConfig = require('./vue-loader.conf');
@@ -23,6 +24,13 @@ const createLintingRule = () => ({
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        eslint: {},
+      },
+    }),
+  ],
   entry: {
     app: ['url-search-params-polyfill', './src/main.js'],
     login: './src/login.js',
@@ -53,6 +61,15 @@ module.exports = {
       vue$: 'vue/dist/vue.esm.js',
       '@': resolve('src'),
     },
+    fallback: {
+      // prevent webpack from injecting mocks to Node native modules
+      // that does not make sense for the client
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+    },
   },
   module: {
     rules: [
@@ -73,26 +90,38 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10000,
+          },
+        },
+        generator: {
+          filename: utils.assetsPath('img/[name].[hash:7][ext]'),
         },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]'),
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10000,
+          },
+        },
+        generator: {
+          filename: utils.assetsPath('media/[name].[hash:7][ext]'),
         },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10000,
+          },
+        },
+        generator: {
+          filename: utils.assetsPath('fonts/[name].[hash:7][ext]'),
         },
       },
       {
@@ -119,17 +148,5 @@ module.exports = {
         ],
       },
     ],
-  },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
   },
 };
